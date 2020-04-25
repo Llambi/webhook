203a8f64a58502d12ba3e8d8fc5c8b737b8edd9b
Santiago Martín Agra <santiagomartinagra@gmail.com>
2020-04-25 22:03:21 +0200

    refactor: how we check the compatible events in 'ping'

--- src/reactions/github/ping.ts
+++ src/reactions/github/ping.ts
@@ -10,12 +10,13 @@ export class Ping extends Reaction<PingPayload> {
 		payload,
 		event,
 	}: ReactionCanHandleOptions<PingPayload>): boolean {
+		const compatibleEvents = ['star', 'fork', 'pull_request', 'issues'];
+
 		return (
 			event === 'ping' &&
-			(payload.hook.events.includes('star') ||
-				payload.hook.events.includes('fork') ||
-				payload.hook.events.includes('pull_request') ||
-				payload.hook.events.includes('issues'))
+			payload.hook.events.some((hookEvent) =>
+				compatibleEvents.includes(hookEvent),
+			)
 		);
 	}
 	getStreamLabsMessage({
